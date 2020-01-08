2268e35c17384aaea277f987dbf2cdcb2076161e
jackie2009 <jackie_2009@163.com>
2020-01-09 01:20:46 +0800

    获得基础反射图

---
+++ PlanarSSR.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class PlanarSSR : MonoBehaviour
+{
+	public RenderTexture worldPosTex;
+	public static int rtSize = 256;
+	private Material matss;
+	public ComputeShader computeShader;
+	public bool dispatchIndirectMode = false;
+    public float waterHeight;
+	public RenderTexture reflectTex;
+ 	// Use this for init
+    int kernelMain;
+    private int kernelCSClear;
+    private ComputeBuffer indirectArguments;
+    private Camera camera;
+	private void Start()
+	{
+		initRes();
+		initComputeShader();
+	}
+
+	private void initRes()
+	{
+		  camera = GetComponent<Camera>();
+		 matss=new Material(Shader.Find("Unlit/SSToWorldPos"));//如果发布程序 这里需要用resources 或assetbundle 加载
+         worldPosTex = RenderTexture.GetTemporary(rtSize, rtSize, 16, RenderTextureFormat.ARGBFloat);
+         reflectTex = new RenderTexture(rtSize, rtSize, 24,RenderTextureFormat.R8);
+         reflectTex.enableRandomWrite = true;
+         reflectTex.Create();
+         Shader.SetGlobalTexture("WaterPssrTex", reflectTex);
+	}
+
+	private void initComputeShader()
+	{
+		print(SystemInfo.supportsComputeShaders);
+		kernelMain =  computeShader.FindKernel("CSMain");
+		kernelCSClear =  computeShader.FindKernel("CSClear");
+
+		computeShader.SetTexture(kernelMain,"Result",reflectTex);
+		computeShader.SetTexture(kernelCSClear,"Result",reflectTex);
+		computeShader.SetTexture(kernelMain,"inputTexture", worldPosTex);
+		 
+		indirectArguments= new ComputeBuffer(1, sizeof(uint) * 3, ComputeBufferType.IndirectArguments);
+		indirectArguments.SetData(new uint[]{(uint)(rtSize / 8), (uint)(rtSize / 8), 1});
+	}
+
+	private void Update()
+	{
+		updateCameraRay();
+	    UpdateComputeShader();
+        
+    }
+
+	private void updateCameraRay()
+	{
+		 
+		Matrix4x4 frustumCorners = Matrix4x4.identity;
+		float fovWHalf = camera.fieldOfView * 0.5f;
+		Vector3 toRight = camera.transform.right * camera.nearClipPlane * Mathf.Tan(fovWHalf * Mathf.Deg2Rad) * camera.aspect;
+		Vector3 toTop = camera.transform.up * camera.nearClipPlane * Mathf.Tan(fovWHalf * Mathf.Deg2Rad);
+		Vector3 topLeft = (camera.transform.forward * camera.nearClipPlane - toRight + toTop);
+		float camScale = topLeft.magnitude * camera.farClipPlane / camera.nearClipPlane;
+		topLeft.Normalize();
+		topLeft *= camScale;
+		Vector3 topRight = (camera.transform.forward * camera.nearClipPlane + toRight + toTop);
+		topRight.Normalize();
+		topRight *= camScale;
+		Vector3 bottomRight = (camera.transform.forward * camera.nearClipPlane + toRight - toTop);
+		bottomRight.Normalize();
+		bottomRight *= camScale;
+		Vector3 bottomLeft = (camera.transform.forward * camera.nearClipPlane - toRight - toTop);
+		bottomLeft.Normalize();
+		bottomLeft *= camScale;
+		frustumCorners.SetRow(2, topLeft);
+		frustumCorners.SetRow(3, topRight);
+		frustumCorners.SetRow(1, bottomRight);
+		frustumCorners.SetRow(0, bottomLeft);
+		matss.SetMatrix("frustumCorners", frustumCorners);
+	}
+
+	void UpdateComputeShader() {
+        
+		computeShader.SetVector("cmrPos",transform.position);
+		if(dispatchIndirectMode==false)
+        computeShader.Dispatch(kernelCSClear, rtSize / 8, rtSize / 8, 1);
+		else
+			computeShader.DispatchIndirect(kernelCSClear,indirectArguments);
+        computeShader.SetFloat("WaterHeight", waterHeight);
+
+        var m = Camera.main.projectionMatrix * Camera.main.worldToCameraMatrix;
+      
+        //高版本 可用  computeShader.SetMatrix("matrix_VP", m); 代替 下面数组传入
+        float[] mlist = new float[] {
+            m.m00,m.m10,m.m20,m.m30,
+           m.m01,m.m11,m.m21,m.m31,
+            m.m02,m.m12,m.m22,m.m32,
+            m.m03,m.m13,m.m23,m.m33
+        };
+ 
+          
+        computeShader.SetFloats("matrix_VP", mlist);
+
+        if(dispatchIndirectMode==false)
+		 computeShader.Dispatch(kernelMain,rtSize/8, rtSize / 8,1);
+       else
+		computeShader.DispatchIndirect(kernelMain,indirectArguments);
+
+    }
+	private void OnDestroy()
+	{
+		RenderTexture.ReleaseTemporary(worldPosTex);
+		 
+	}
+
+	private void OnRenderImage(RenderTexture src, RenderTexture dest)
+	{
+		 
+			Graphics.Blit(src, dest);
+			Graphics.Blit(src, worldPosTex, matss);
+		 
+		
+	}
+}
---
+++ SSToWorldPos.shader
@@ -0,0 +1,99 @@
+﻿// Upgrade NOTE: replaced '_CameraToWorld' with 'unity_CameraToWorld'
+
+// Upgrade NOTE: replaced '_CameraToWorld' with 'unity_CameraToWorld'
+
+// Upgrade NOTE: replaced '_CameraToWorld' with 'unity_CameraToWorld'
+// Upgrade NOTE: replaced 'mul(UNITY_MATRIX_MVP,*)' with 'UnityObjectToClipPos(*)'
+
+Shader "Unlit/SSToWorldPos"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+	}
+	SubShader
+	{
+		Tags { "RenderType"="Opaque" }
+		LOD 100
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+	 
+			
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+			 
+				 
+				float4 vertex : SV_POSITION;
+			 float3 ray : TEXCOORD1;
+			};
+
+			uniform float4x4	frustumCorners;
+			uniform sampler2D _CameraDepthTexture;
+			v2f vert (appdata v)
+			{
+				v2f o;
+
+
+			 o.vertex = UnityObjectToClipPos(v.vertex);
+			 
+			 o.uv = v.uv;
+			
+			 uint x = (uint)(o.uv.x * 1.5);
+			 uint y = (uint)(o.uv.y * 1.5);
+			 half index = y * 2 + x;//取得索引
+			 o.ray = frustumCorners[index].xyz;//根据在程序中计算好的顶点z值作为索引
+			 
+			 
+				return o;
+			}
+			
+			fixed4 frag (v2f i) : SV_Target
+			{
+
+		 
+	 float depth = SAMPLE_DEPTH_TEXTURE(_CameraDepthTexture, i.uv);
+			depth = Linear01Depth(depth);
+			 
+		  fixed4 worldPos = fixed4(depth*i.ray, 1);
+
+			worldPos.xyz += _WorldSpaceCameraPos;
+			if (depth > 0.9) return 0;
+ 
+return worldPos;
+/*
+				// float  ray = mul(UNITY_MATRIX_MV, v.vertex).xyz * float3(-1, -1, 1);
+				 float  ray = mul(UNITY_MATRIX_MV, v.vertex).xyz * float3(-1, -1, 1);
+				 ray = ray * (_ProjectionParams.z / ray.z);
+			//screen position of UV / depth
+	 
+
+			float depth = SAMPLE_DEPTH_TEXTURE(_CameraDepthTexture, i.uv);
+			depth = Linear01Depth(depth);
+
+			float4 viewPos = float4(ray * depth, 1);
+			float3 worldPos = mul(_CameraToWorld, viewPos).xyz;
+				// sample the texture
+			fixed4 col = 1;
+				col.rgb= worldPos;
+				// apply fog
+				//UNITY_APPLY_FOG(i.fogCoord, col);
+				return col;
+				*/
+			}
+			ENDCG
+		}
+	}
+}
